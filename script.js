325340e23065f5072ec9086d5b81e3c94f2e4e8d
manolorialv <38545199+manolorialv@users.noreply.github.com>
2026-04-22 02:05:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+document.getElementById('menu-btn').addEventListener('click', function() {
+    const nav = document.querySelector('.md:flex');
+    nav.classList.toggle('hidden');
+    nav.classList.toggle('flex');
+    nav.classList.toggle('flex-col');
+    nav.classList.toggle('absolute');
+    nav.classList.toggle('top-16');
+    nav.classList.toggle('left-0');
+    nav.classList.toggle('w-full');
+    nav.classList.toggle('bg-slate-900');
+    nav.classList.toggle('p-6');
+});
